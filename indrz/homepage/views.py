a8466b8f1a85a0f2ff5cb393f5c18b6a6d85ee07
Michael Diener <mdiener21@gmail.com>
2016-06-07 13:15:25 +0200

    first big commit homepage app WU

---
+++ indrz/homepage/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+
+def view_map(request, *args, **kwargs):
+    context = {}
+    if request.method == 'GET':
+        map_name = kwargs.pop('map_name', None)
+        building_id, = request.GET.get('buildingid', 1),
+        space_id, = request.GET.get('spaceid', 0),
+        zoom_level, = request.GET.get('zlevel', 18),
+        route_from, = request.GET.get('route_from', ''),
+        route_to, = request.GET.get('route_to', ''),
+        centerx, = request.GET.get('centerx', 0),
+        centery, = request.GET.get('centery', 0),
+        floor_num, = request.GET.get('floor', 0),
+
+        context.update({
+            'map_name': map_name,
+            'building_id': building_id,
+            'space_id': int(space_id),
+            'zoom_level': zoom_level,
+            'route_from': route_from,
+            'route_to': route_to,
+            'centerx':  float(centerx),
+            'centery': float(centery),
+            'floor_num': int(floor_num)
+        })
+
+    return render(request, context=context, template_name='homepage/wu-map.html')
+
+def show_help(request):
+    return render(request, template_name='homepage/wu-help.html')
