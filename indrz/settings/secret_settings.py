9d5f0062eec12d7756d34c05af72a6cfd05b3188
Michael Diener <mdiener21@gmail.com>
2016-07-27 17:16:01 +0200

    updated translation files and javascript translations

--- indrz/settings/secret_settings.py
+++ indrz/settings/secret_settings.py
@@ -1,3 +1,8 @@
-db_user = 'indrz-pg'
-db_pwd = '4JSq90JoHJkJWjZ0Snox'
+# db_user = 'indrz-pg'
+# db_pwd = '4JSq90JoHJkJWjZ0Snox'
+
+# live redhat Postgresql 9.5 DB
+db_user = 'indrz-wu'
+db_pwd = 'yywBKIY155TktYz8Vo48'
+
 secret_key = 'ghk7&jaJlCYQ0ribu=3*mr+b00+%6)jpltoft!@+#dx=us9!^^vch$zl5_'
