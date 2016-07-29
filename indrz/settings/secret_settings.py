e833137fabc489f01534d185a52baed62809dfe8
Michael Diener <mdiener21@gmail.com>
2016-07-29 11:54:49 +0200

    removed secret settings

--- indrz/settings/secret_settings.py
+++
@@ -1,8 +0,0 @@
-# db_user = 'indrz-pg'
-# db_pwd = '4JSq90JoHJkJWjZ0Snox'
-
-# live redhat Postgresql 9.5 DB
-db_user = 'indrz-wu'
-db_pwd = 'yywBKIY155TktYz8Vo48'
-
-secret_key = 'ghk7&jaJlCYQ0ribu=3*mr+b00+%6)jpltoft!@+#dx=us9!^^vch$zl5_'
