a242fc544bfe0338c43dc31a16c0c55bd86c01cf
Michael Diener <mdiener21@gmail.com>
2016-08-10 15:04:33 +0200

    lint fixes

--- indrz/maps/templates/maps/indrzLayers.js
+++ indrz/maps/templates/maps/indrzLayers.js
@@ -1,18 +1,19 @@
-function createWmsLayer(layerName, geoserverLayer, floorNumber, isVisible, zIndexValue){
+function createWmsLayer(layerName, geoserverLayer, floorNumber, isVisible, zIndexValue) {
     var newWmsLayer = new ol.layer.Image({
-    source: new ol.source.ImageWMS({
-        url: baseUrlWms,
-        params: {'LAYERS': geoserverLayer},
-        serverType: 'geoserver',
-        crossOrigin: ''
-    }),
-    visible: isVisible,
-    name: layerName,
-    floor: floorNumber,
-    type: "floor",
-    zIndex: zIndexValue
+        source: new ol.source.ImageWMS({
+            url: baseUrlWms,
+            params: {'LAYERS': geoserverLayer},
+            serverType: 'geoserver',
+            crossOrigin: ''
+        }),
+        visible: isVisible,
+        name: layerName,
+        floor: floorNumber,
+        type: "floor",
+        zIndex: zIndexValue,
+        crossOrigin: "anonymous"
 
-});
+    });
 
     return newWmsLayer;
 }
@@ -31,31 +32,32 @@ wmsE03 = createWmsLayer('wmsE03','indrz:e03', '3', 'false', 3 );
 var OsmBackLayer = new ol.layer.Tile({
     source: new ol.source.OSM(),
     visible: true,
-    type:"background"});
+    type: "background"
+});
 
 
-$.ajax('/api/v1/buildings/' + building_id +'/')
-    .then(function(response) {
+$.ajax('/api/v1/buildings/' + building_id + '/')
+    .then(function (response) {
         building_info = response;
-        for(var i=0; i< response.num_floors; i++){
+        for (var i = 0; i < response.num_floors; i++) {
             var geojsonFormat = new ol.format.GeoJSON();
             var floor_info = response.buildingfloor_set[i];
             var features = geojsonFormat.readFeatures(floor_info.buildingfloorspace_set,
                 {featureProjection: 'EPSG:4326'});
-            var spaces_source =  new ol.source.Vector();
+            var spaces_source = new ol.source.Vector();
             spaces_source.addFeatures(features);
 
             var floor_spaces_vector = new ol.layer.Vector({
                 source: spaces_source,
-                style:  new ol.style.Style({
+                style: new ol.style.Style({
                     fill: new ol.style.Fill({
                         color: 'rgba(255, 255, 255, 0.6)'
                     }),
                     stroke: new ol.style.Stroke({
-                      color: 'grey',
-                      width: 1
+                        color: 'grey',
+                        width: 1
                     })
-                  }),
+                }),
                 title: "spaces",
                 name: "spaces",
                 floor_num: floor_info.floor_num,
@@ -66,21 +68,21 @@ $.ajax('/api/v1/buildings/' + building_id +'/')
             floor_layers.push(floor_spaces_vector);
             appendFloorNav(floor_info, i);
         }
-        if(space_id=="0"){
-            for(var i=0; i< floor_layers.length; i++) {
-                if(active_floor_num == floor_layers[i].getProperties().floor_num){
+        if (space_id == "0") {
+            for (var i = 0; i < floor_layers.length; i++) {
+                if (active_floor_num == floor_layers[i].getProperties().floor_num) {
                     activateLayer(i);
                 }
             }
         }
-});
+    });
 
 
-function appendFloorNav(floor_info, index){
+function appendFloorNav(floor_info, index) {
     $("#floor-links").prepend("<li>" +
-    "<a href='#' onclick='activateLayer(" +
-    index +
-    ");' id='action-1'>"+ floor_info.short_name +"</a></li>");
+        "<a href='#' onclick='activateLayer(" +
+        index +
+        ");' id='action-1'>" + floor_info.short_name + "</a></li>");
     // Add flour to mobile ui element
-    $("#floor-links-select").prepend("<option value='"+ index +"'>" + floor_info.short_name + "</option>");
+    $("#floor-links-select").prepend("<option value='" + index + "'>" + floor_info.short_name + "</option>");
 }
