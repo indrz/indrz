d78c3de565d714f121eb3125554a1d71dad3f756
mdiener21 <root@baywatch.wu.ac.at>
2016-09-22 15:23:41 +0200

    update wsgi production

--- indrz/indrz/wsgi.py
+++ indrz/indrz/wsgi.py
@@ -12,6 +12,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.production_settings")
 
 application = get_wsgi_application()
