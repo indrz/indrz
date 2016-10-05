8ad296768fd50b3f7e51dae21ece61d336cf842b
Michael Diener <mdiener21@gmail.com>
2016-10-05 13:06:25 +0200

    fixed set center map for sharing

--- indrz/homepage/templates/homepage/initialize.js
+++ indrz/homepage/templates/homepage/initialize.js
@@ -1,12 +1,30 @@
 $(document).ready(function () {
     function initialize() {
+
+
+        if (centerx.indexOf(",")) {
+
+                var view = map.getView();
+                view.setCenter([centerx.replace(',', '.'), centery.replace(',', '.')]);
+                view.setZoom(zoom_level);
+            }
+
+
+        if (centerx != 0 && centery != 0 && isNaN(centerx) == false) {
+
+                var view = map.getView();
+                view.setCenter([centerx, centery]);
+                view.setZoom(zoom_level);
+            }
+
         if (floor_layers.length > 0) {
+
             if (route_from != '' && route_to != '') {
                 $("#route-from").val(route_from);
                 $("#route-to").val(route_to);
                 $("#submitForm").submit();
             } else if (centerx != 0 && centery != 0 && isNaN(centerx) == false) {
-                console.log("init state is : " + centerx);
+
                 var view = map.getView();
                 view.setCenter([centerx, centery]);
                 view.setZoom(zoom_level);
