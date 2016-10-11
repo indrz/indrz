d47999442700ea74f846769148911d58cb2708b7
Michael Diener <mdiener21@gmail.com>
2016-10-11 16:49:37 +0200

    updated serializer to include depth = 1 poi categories

--- indrz/poi_manager/serializers.py
+++ indrz/poi_manager/serializers.py
@@ -4,10 +4,13 @@ from poi_manager.models import Poi, PoiCategory
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 class PoiSerializer(GeoFeatureModelSerializer):
+
+
     class Meta:
         model = Poi
         geo_field = 'geom'
-        #fields = ('floor_num', 'short_name')
+        depth = 1
+        fields = ('name', 'floor_num', 'fk_poi_category')
 
 
 class PoiCategorySerializer(serializers.ModelSerializer):
