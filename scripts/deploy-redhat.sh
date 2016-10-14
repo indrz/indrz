136c0d40477ecb78b22f674b81a4fbfb5127ffec
Michael Diener <mdiener21@gmail.com>
2016-10-14 12:32:37 +0200

    added settings

---
+++ scripts/deploy-redhat.sh
@@ -0,0 +1,8 @@
+rm -rf /opt/deploy_django/indrz
+rm -rf /opt/deploy_django/static
+rm -rf /opt/deploy_django/media
+
+# copy source files to destination folder
+cp -R /opt/indrz-wu/indrz /opt/django-deploy/indrz
+
+
