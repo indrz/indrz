aa654dbee40ff0df15766ad3073ebf36211129ab
Michael Diener <mdiener21@gmail.com>
2016-10-28 10:11:33 +0200

    fix paths

--- scripts/deploy-redhat.sh
+++ scripts/deploy-redhat.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
-rm -rf /opt/deploy-django/indrz
-rm -rf /opt/deploy-django/static
-rm -rf /opt/deploy-django/media
 
-mkdir /opt/deploy-django/media
+cd /opt/indrz-wu
+
+rm -rf /opt/django-deploy/indrz
+rm -rf /opt/django-deploy/static
+rm -rf /opt/django-deploy/media
+
+mkdir /opt/django-deploy/media
 
 # copy source files to destination folder
 cp -R /opt/indrz-wu/indrz /opt/django-deploy/indrz
 
 
-cd /opt/deploy-django/indrz
+cd /opt/django-deploy/indrz
 source /opt/venvs/py3uwsgi/bin/activate
 python manage.py collectstatic
 
