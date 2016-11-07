7491494d569ce99f0d6165a109bad4838ad783b6
Michael Diener <mdiener21@gmail.com>
2016-11-07 12:52:20 +0100

    update map layout and template layout designs

--- indrz/homepage/templates/homepage/indrzMapConf.js
+++ indrz/homepage/templates/homepage/indrzMapConf.js
@@ -22,3 +22,66 @@ var centery = "{{centery}}";
 var StartCenterX = 1826602.5273166203;
 var StartCenterY = 6142514.2285252055;
 
+
+var building_id = 1;
+
+var searchValues = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    // prefetch: baseApiUrl + 'spaces/search/?format=json',
+    prefetch: '/autocomplete/%QUERY?.format=json',
+    remote: {
+      url: '/autocomplete/%QUERY?.format=json',
+      wildcard: '%QUERY'
+    }
+});
+
+// passing in `null` for the `options` arguments will result in the default
+// options being used
+$('#search-input').typeahead(null, {
+    hint: true,
+    highlight: true,
+    minLength: 1,
+    name: 'search-field',
+    limit: 100,
+    source: searchValues
+});
+
+
+$("#submitFormSearch").submit(function (event) {
+    //              alert( "Handler for .submit() called."  );
+
+    //            var buildingid = buildingNantesId;
+    var searchText = $('#search-input').val();
+    searchIndrz(building_id, searchText);
+    event.preventDefault();
+});
+
+$("#showPoi").submit(function (event) {
+   // alert( "Handler for .submit() called." + $('#poi-input').val()   );
+    var poiName = $('#poi-input').val();
+    searchPoi(building_id, poiName);
+    event.preventDefault();
+    });
+
+       var roomNums = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    prefetch: baseApiUrl + 'spaces/search/?format=json'
+});
+// passing in `null` for the `options` arguments will result in the default
+// options being used
+$('#rooms-prefetch .typeahead').typeahead(null, {
+    name: 'countries',
+    limit: 100,
+    source: roomNums
+});
+$("#submitForm").submit(function (event) {
+    {#  alert( "Handler for .submit() called."  );#}
+    var startNum = $('#route-from').val();
+    var endNum = $('#route-to').val();
+    var rType = $("input:radio[name=typeRoute]:checked").val();
+    addRoute(startNum, endNum, rType);
+    event.preventDefault();
+});
+
