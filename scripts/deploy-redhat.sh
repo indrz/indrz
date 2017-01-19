f10c5a2998a6748e3d9d61eebdd6a0ae229ce26c
Michael Diener <mdiener21@gmail.com>
2017-01-19 13:26:52 +0100

    update deploy script

--- scripts/deploy-redhat.sh
+++ scripts/deploy-redhat.sh
@@ -2,6 +2,9 @@
 
 cd /opt/indrz-wu
 
+# copy all server made translation changes to temp dir
+cp -R /opt/django-deploy/indrz/locale/de /home/mdiener/temp/
+
 rm -rf /opt/django-deploy/indrz
 rm -rf /opt/django-deploy/static
 rm -rf /opt/django-deploy/media
@@ -11,11 +14,15 @@ mkdir /opt/django-deploy/media
 # copy source files to destination folder
 cp -R /opt/indrz-wu/indrz /opt/django-deploy/indrz
 
+# restore server made translations after deploy
+rm -rf /opt/django-deploy/indrz/locale/de
+cp -R /home/mdiener/temp/de /opt/django-deploy/indrz/locale/de
+
 
 cd /opt/django-deploy/indrz
 source /opt/venvs/py3uwsgi/bin/activate
 python manage.py collectstatic
 
 cd /opt/indrz-wu/
-systemctl restart uwsgi
+supervisorctl reload
 systemctl reload nginx
